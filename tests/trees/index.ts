b5f0297cc1ad6a901cbca18ace5cd62c5c39cb96
Daniel Sales <dsa21may@gmail.com>
2021-09-30 16:17:43 +0200

    add removeChildren func

--- tests/trees/index.ts
+++ tests/trees/index.ts
@@ -1,6 +1,13 @@
+import { Tree } from "@app/tree";
+
 export {
   genFileMock1,
   NODE_WITHOUT_CHILDREN,
   NODE_WITH_CHILDREN,
   ROOT_WITH_TWO_DUPLICATED_CHILDREN,
 } from "./mocks";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function removeChildren( { children, ...obj }: Tree) {
+  return obj;
+}
