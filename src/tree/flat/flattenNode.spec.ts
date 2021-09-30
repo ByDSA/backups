1ac458828b4ed8aa2224eb39ea0b7d34e4795bbb
Daniel Sales <dsa21may@gmail.com>
2021-09-30 16:39:34 +0200

    .

--- src/tree/flat/flattenNode.spec.ts
+++ src/tree/flat/flattenNode.spec.ts
@@ -1,20 +1,20 @@
-import { NODE_WITH_CHILDREN, removeChildren } from "@tests/trees";
+import { removeChildren, ROOT_WITH_CHILDREN } from "@tests/trees";
 import flattenNode from "./flattenNode";
 import FlatTree from "./FlatTree";
 
 describe("flattenNode", () => {
   it("removes children", () => {
-    const actual = flattenNode(NODE_WITH_CHILDREN);
+    const actual = flattenNode(ROOT_WITH_CHILDREN);
 
     expect((<any>actual).children).toBeUndefined();
   } );
 
   it("only root", () => {
     const expected: FlatTree = {
-      ...removeChildren(NODE_WITH_CHILDREN),
+      ...removeChildren(ROOT_WITH_CHILDREN),
       path: "/root",
     };
-    const actual = flattenNode(NODE_WITH_CHILDREN);
+    const actual = flattenNode(ROOT_WITH_CHILDREN);
 
     expect(actual).toStrictEqual(expected);
   } );
