46c93e7421c9c48b15ec040aa5488353ec9daa3f
Daniel Sales <dsa21may@gmail.com>
2021-09-30 12:15:51 +0200

    feat: add 'compare' command

--- src/params/tree/compare.ts
+++ src/params/tree/compare.ts
@@ -1,3 +1,4 @@
+import { compareTree, readTree } from "@app/tree";
 import chalk from "chalk";
 import yargs, { Arguments } from "yargs";
 
@@ -25,5 +26,9 @@ function handler<U>(argv: Arguments<U>) {
   };
 
   console.log(chalk.blue(`Comparing trees: '${config.input1}' and '${config.input2}' ...`));
-  console.log(chalk.yellowBright("Not implemented yet."));
+  const t1 = readTree(config.input1);
+  const t2 = readTree(config.input2);
+  const differences = compareTree(t1, t2);
+
+  console.log(differences);
 }
