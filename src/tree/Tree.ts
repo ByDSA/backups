35aa4371c3a62da4f41c62863b291ecf0a001309
Daniel Sales <dsa21may@gmail.com>
2021-09-30 11:59:10 +0200

    Add 'TreeWithoutChildren'

--- src/tree/Tree.ts
+++ src/tree/Tree.ts
@@ -1,11 +1,18 @@
-type Tree = {
+type TreeWithoutChildren = {
   name: string;
   size: number;
   hash: string;
   modificatedAt: number;
   createdAt: number;
   accessedAt?: number;
+};
+
+type Tree = TreeWithoutChildren & {
   children?: Tree[];
 };
 
 export default Tree;
+
+export {
+  TreeWithoutChildren,
+};
