ae1da8864225c390f3d470b080c3d0a7b25f70ea
Daniel Sales <dsa21may@gmail.com>
2021-09-30 12:03:31 +0200

    Add 'plainPathsMap'

--- src/tree/utils.ts
+++ src/tree/utils.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-import Tree from "./Tree";
+import Tree, { TreeWithoutChildren } from "./Tree";
 
-export type PlainTree = Tree & {
+export type PlainTree = TreeWithoutChildren & {
   path: string;
 };
 
@@ -10,15 +10,35 @@ export function plain(t: Tree, basePath: string = "", acc: PlainTree[] = []): Pl
     return acc;
 
   for (const c of t.children) {
-    const plainTree = {
+    const plainTree: PlainTree = {
       ...c,
       path: `${basePath}/${c.name}`,
     };
 
-    delete plainTree.children;
     acc.push(plainTree);
 
     plain(c, `${basePath}/${c.name}`, acc);
+  }
+
+  return acc;
+}
+
+type Path = string;
+type PathsMap = Map<Path, PlainTree>;
+export function plainPathsMap(t: Tree, basePath: string = "", acc: PathsMap = new Map<Path, PlainTree>()): PathsMap {
+  if (!t.children)
+    return acc;
+
+  for (const c of t.children) {
+    const path = `${basePath}/${c.name}`;
+    const plainTreePath: PlainTree = {
+      ...c,
+      path,
+    };
+
+    acc.set(path, plainTreePath);
+
+    plainPathsMap(c, `${basePath}/${c.name}`, acc);
   }
 
   return acc;
